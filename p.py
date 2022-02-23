33e72cb27a710ac4adbe84023b86a65cf7675ef3
Ratmil Torres <ratmil@gmail.com>
2022-02-23 18:37:30 -0300

    Added tests

--- p.py
+++ p.py
@@ -1,2 +1,12 @@
-def sum(a, b):
+def sum_numbers(a, b):
 	return a + b
+	
+def test_sum_numbers():
+	assert sum_numbers(3, 4) == 7
+	assert sum_numbers(1, 2) == 3
+	assert sum_numbers(2, 2) == 4
+	assert sum_numbers(1, 3) == 4
+	
+def test_sum_numbers2():
+	assert sum_numbers(1, 2) == 3
+	assert sum_numbers(2, 2) == 4
